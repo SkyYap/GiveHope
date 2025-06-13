c54b20ada94051e87bd83fbc581488e323a7191c
SkyYap <32660903+SkyYap@users.noreply.github.com>
2025-06-14 01:48:27 +0800

    build: remove search projects at navigation bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -41,15 +41,6 @@ export const Header: React.FC = () => {
 
           {/* Search and Connect */}
           <div className="flex items-center space-x-4">
-            <div className="hidden sm:block relative">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-              <input
-                type="text"
-                placeholder="Search projects..."
-                className="pl-10 pr-4 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
-            </div>
-            
             <ConnectButton.Custom>
               {({
                 account,
@@ -162,13 +153,6 @@ export const Header: React.FC = () => {
                     {item.name}
                   </Link>
                 ))}
-                <div className="px-3 py-2">
-                  <input
-                    type="text"
-                    placeholder="Search projects..."
-                    className="w-full pl-3 pr-4 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                </div>
               </div>
             </motion.div>
           )}
